626d77fee4c4ed527ebfa47854018821837647d5
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-17 15:05:18 +0200

    Fix several issues mentioned at Puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,14 @@
-class dpkg {
+class dpkg inherits dpkg::params {
   file { '/etc/cron.d/get-selections':
-    owner  => root,
-    group  => root,
+    owner  => 'root',
+    group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/dpkg/common/etc/cron.d/get-selections',
   }
 
   file { '/etc/logrotate.d/get-selections':
-    owner  => root,
-    group  => root,
+    owner  => 'root',
+    group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/dpkg/common/etc/logrotate.d/get-selections',
   }
